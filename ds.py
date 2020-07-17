69f881e3770dfaa377c476a1857286769c3a602c
sumanth2k <68301774+sumanth2k@users.noreply.github.com>
2020-07-17 19:25:43 +0530

    Create ds.py

---
+++ ds.py
@@ -0,0 +1,17 @@
+list1=["c","c++","r"]
+list1.append("python")
+print(list1)
+
+
+tup1=(1,2,3,4,5,6)
+print(tup1[3])
+
+
+
+dict1={"apple":"red","banana":"yellow","grapes":"green"}
+print(dict1)
+dict1.pop("banana")
+del dict1["grapes"]
+print(dict1)
+dict1["mango"]="yellow"
+print(dict1)
